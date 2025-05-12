1ed0a33ea9c62914e2ed058fcc256ddc945f8170
FilippoLucchesi <f.lucchesi14@studenti.unipi.it>
2025-05-12 13:03:58 +0200

    library setup

---
+++ Project/test.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cnl/all.h>
+
+int main() {
+    using fixed_8_4 = cnl::scaled_integer<int32_t, cnl::power<-16>>; // 8 bits total, 4 fractional bits
+
+    fixed_8_4 a = 32767.3; // Maximum value for fixed_8_4
+    fixed_8_4 b = 32768;
+    fixed_8_4 sum = a + b;
+
+    std::cout << "a = " << a << std::endl;
+    std::cout << "b = " << b << std::endl;
+    std::cout << "sum = " << sum << std::endl;
+
+    return 0;
+}
